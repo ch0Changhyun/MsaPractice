9c6b0a25e4c420dab7eca4df7d1ac73c707e26d2
chc <311mikecho@gmail.com>
2024-12-02 01:25:44 +0900

    분산추적

--- com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -1,16 +1,21 @@
 package com.spring_cloud.eureka.client.order;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 @RestController
-@RequestMapping("/order")
+@RequiredArgsConstructor
 public class OrderController {
 
-    @GetMapping
-    public String getOrder() {
-        return "Order details";
-    }
-}
+    private final OrderService orderService;
 
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable String orderId) {
+        return orderService.getOrder(orderId);
+    }
+
+}
